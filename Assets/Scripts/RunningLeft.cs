e4413c78f8c0a6ce2dac7d53c0ac39648e714410
Cherry <d6m8@ugrad.cs.ubc.ca>
2017-01-21 01:29:41 -0800

    cleaning up files/folder, renamed script

--- Assets/Scripts/RunningLeft.cs
+++ Assets/Scripts/RunningLeft.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RunningRight : MonoBehaviour {
-    // Use this for initialization
-    void Start () {
+public class RunningLeft : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 position = this.transform.position;
-        position.x -= 0.05f; 
-        this.transform.position = position; 
+        position.x -= 0.05f;
+        this.transform.position = position;
     }
 }
