56ea92c59ac8f5cf3735cbfeb2836764a9261ee5
Cherry <d6m8@ugrad.cs.ubc.ca>
2017-01-21 09:44:58 -0800

    Adding enemy spawn and more enemies

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour {
+    public GameObject[] enemies; 
+    //public GameObject enemy;
+    public float spawnTime = 5f;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("spawnEnemies", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void spawnEnemies()
+    {
+        //enemy = GameObject.Find("Red");
+        //Instantiate(enemy, new Vector3(12, -3, 0), Quaternion.identity);
+        Instantiate(enemies[UnityEngine.Random.Range(0, enemies.Length-1)], new Vector3(12, -3, 0), Quaternion.identity);
+
+    }
+}
--- Assets/Scripts/WaveButton.cs
+++ Assets/Scripts/WaveButton.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaveButton : MonoBehaviour {
 
-    public GameObject red; 
+    public GameObject red;
+    public GameObject[] allEnemies;
+    public Vector3 dist;
+    public Transform pos;
+
+    private Button button;
+
+    void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        button.onClick.AddListener(Clicked);
+    }
+
+    void Clicked()
+    {
+        Debug.Log("Hello");
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,6 +36,8 @@ public class WaveButton : MonoBehaviour {
     {
         red = GameObject.Find("Red");
         red.transform.Translate(Vector3.up); 
-        
+
     }
+
+
 }
