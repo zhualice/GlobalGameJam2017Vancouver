eb146fe868be820ca89ce265b65d177a9ea53b3a
ZHUALICE <alice-desu@hotmail.com>
2017-01-21 12:01:26 -0800

    still resolving conflicts

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -21,11 +21,8 @@ public class SpawnEnemies : MonoBehaviour {
     {
         //enemy = GameObject.Find("Red");
         //Instantiate(enemy, new Vector3(12, -3, 0), Quaternion.identity);
-<<<<<<< HEAD
         Instantiate(enemies[UnityEngine.Random.Range(0, enemies.Length)], new Vector3(12, -3, 0), Quaternion.identity);
-=======
-        Instantiate(enemies[UnityEngine.Random.Range(0, enemies.Length-1)], new Vector3(12, -3, 0), Quaternion.identity);
->>>>>>> 68c577dba2b421c5271e5b149cdec1ffa7de4946
+
 
     }
 }
