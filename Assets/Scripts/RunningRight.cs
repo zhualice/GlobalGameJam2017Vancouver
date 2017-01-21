b7468b52b670206c1b4f6d1f7623ca4436c9f616
Cherry <d6m8@ugrad.cs.ubc.ca>
2017-01-21 00:54:27 -0800

    adding wave button

---
+++ Assets/Scripts/RunningRight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningRight : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = this.transform.position;
+        position.x -= 0.05f; 
+        this.transform.position = position; 
+    }
+}
---
+++ Assets/Scripts/WaveButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveButton : MonoBehaviour {
+
+    public GameObject red; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void wave()
+    {
+        red = GameObject.Find("Red");
+        red.transform.Translate(Vector3.up); 
+        
+    }
+}
