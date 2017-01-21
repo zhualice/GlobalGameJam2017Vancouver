3f8f93a039017826d611df4dc7e51a8ee80c638c
ZHUALICE <alice-desu@hotmail.com>
2017-01-21 10:51:54 -0800

    Awkward health bar, speech text and fixes

---
+++ Assets/Scripts/AwkwardController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Image = UnityEngine.UI.Image;
+using UnityEngine;
+
+public class AwkwardController : MonoBehaviour {
+    Image AwkwardBar;
+    float tmpHealth; 
+	// Use this for initialization
+	void Start () {
+        //   AwkwardBar = GameObject.Find("HealthCamera").transform.FindChild("AwkCanvas").FindChild("AwkwardBar").GetComponent<Image>(); 	
+        AwkwardBar = GetComponent<Image>();
+    }
+
+    public void changeAwkwardHealth()
+    {
+        tmpHealth = Random.Range(0f, 1f);
+        AwkwardBar.fillAmount = tmpHealth;
+    }
+}
